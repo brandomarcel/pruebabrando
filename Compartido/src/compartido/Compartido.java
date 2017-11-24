bac68a58664613c00def73268aacb9aba16a433d
michu <michu@SMEL>
2017-11-24 14:38:39 -0500

    primer cambio

--- Compartido/src/compartido/Compartido.java
+++ Compartido/src/compartido/Compartido.java
@@ -5,6 +5,8 @@
  */
 package compartido;
 
+import javax.swing.JPanel;
+
 /**
  *
  * @author brand
@@ -15,7 +17,10 @@ public class Compartido {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        
+        System.out.println("primer cambio");
+// TODO code application logic here
     }
     
 }
